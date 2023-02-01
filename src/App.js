c7f36f196dbac1ee5a2baaa3b6b575b57f6c348a
KarenAriniaina <99282731+KarenAriniaina@users.noreply.github.com>
2023-02-01 10:21:11 +0300

    fiche enchere + lien to it

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Accueil from './page/Accueil';
 import HistoriqueEnchere from './page/HistoriqueEnchere';
+import FicheEnchere from './page/FicheEnchere';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Accueil/>} />
       <Route path="/HistoriqueEnchere" element={<HistoriqueEnchere/>} />
+      <Route path="/FicheEnchere/:id" element={<FicheEnchere/>} />
     </Routes>
   );
 }
---
+++ src/Component/DetailEnchere.jsx
@@ -0,0 +1,89 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import React from 'react';
+
+
+const Detail = ({ enchere }) => {
+    return (
+        <div className='row'>
+            <div className='col-md-6'>
+                <img href="../../assets/image/lux11.jpeg" alt='../../assets/image/lux11.jpeg' />
+            </div>
+            <div className='col-md-6'>
+                <p>Nom produit:{enchere.nom}</p>
+                <p>Description : {enchere.description}</p>
+                <p>Prix de base : {enchere.prixDepart}</p>
+                <p>Date mise en enchere : {enchere.date}</p>
+                <p>Date  fin enchere : {enchere.dateFin}</p>
+                <p>Liste client :</p>
+                <ListeEncherir encherir={enchere.encherir} />
+            </div>
+        </div>
+    );
+
+}
+function Header(props) {
+    let r = props.header;
+    return (
+        <thead>
+                {
+                    r.map(elements => <th>{elements}</th>)
+                }
+        </thead>
+    );
+}
+function Tablebody(props) {
+    let body = props.content;
+    console.log(body);
+    return (
+
+        <tr>
+            <td>{body.Client}</td>
+            <td>{body.Montant}</td>
+        </tr>
+
+    );
+}
+
+function ListeEncherir(props) {
+    const header = ["Nom", "Montant"];
+    if (props.encherir != undefined) {
+        return (
+            <table className="table "  >
+                <Header header={header} />
+                <tbody>
+                    {
+                        props.encherir.map(elements => <Tablebody content={elements} />)
+                    }
+                </tbody>
+            </table>
+        );
+    }
+    else {
+        return (
+            <table className="table "  >
+                <Header header={header} />
+            </table>
+        );
+    }
+}
+
+
+export const DetailEnchere = ({ Liste }) => {
+    console.log(Liste);
+    return (
+        <div className='row'>
+            <div className='col-md-2'></div>
+            <div className="col-md-8">
+                <div className="row">
+                    {
+                        <Detail enchere={Liste} />
+                    }
+                </div>
+            </div>
+            <div className='col-md-2'></div>
+        </div>
+    )
+};
+
+export default DetailEnchere;
